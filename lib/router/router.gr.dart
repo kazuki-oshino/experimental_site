3c5a61303579b8beea99979c31d8dab051c1c102
kazuikios <slash0juice@gmail.com>
2021-10-28 14:44:54 +0900

    move package

--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -8,78 +8,85 @@
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i5;
+import 'package:auto_route/auto_route.dart' as _i3;
 import 'package:experimental_site/main.dart' as _i1;
-import 'package:experimental_site/views/auto_route/auto_route_page1.dart'
-    as _i2;
-import 'package:experimental_site/views/auto_route/auto_route_page2.dart'
-    as _i3;
-import 'package:experimental_site/views/auto_route/auto_route_page3.dart'
-    as _i4;
-import 'package:flutter/material.dart' as _i6;
+import 'package:experimental_site/views/view.dart' as _i2;
+import 'package:flutter/material.dart' as _i4;
 
-class RootRouter extends _i5.RootStackRouter {
-  RootRouter([_i6.GlobalKey<_i6.NavigatorState>? navigatorKey])
+class RootRouter extends _i3.RootStackRouter {
+  RootRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i5.PageFactory> pagesMap = {
+  final Map<String, _i3.PageFactory> pagesMap = {
     MyHomeRoute.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
+      return _i3.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i1.MyHomePage());
     },
     AutoRouteRoute1.name: (routeData) {
-      return _i5.CustomPage<dynamic>(
+      return _i3.CustomPage<dynamic>(
           routeData: routeData,
           child: const _i2.AutoRoutePage1(),
-          transitionsBuilder: _i5.TransitionsBuilders.fadeIn,
+          transitionsBuilder: _i3.TransitionsBuilders.fadeIn,
           durationInMilliseconds: 1000,
           opaque: true,
           barrierDismissible: false);
     },
     AutoRouteRoute2.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i3.AutoRoutePage2());
+      return _i3.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i2.AutoRoutePage2());
     },
     AutoRouteRoute3.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i4.AutoRoutePage3());
+      return _i3.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i2.AutoRoutePage3());
+    },
+    FlutterSvgSample.name: (routeData) {
+      return _i3.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i2.FlutterSvgSample());
     }
   };
 
   @override
-  List<_i5.RouteConfig> get routes => [
-        _i5.RouteConfig(MyHomeRoute.name, path: '/'),
-        _i5.RouteConfig(AutoRouteRoute1.name, path: '/main1/gorilla'),
-        _i5.RouteConfig(AutoRouteRoute2.name, path: '/auto-route-page2'),
-        _i5.RouteConfig(AutoRouteRoute3.name, path: '/auto-route-page3')
+  List<_i3.RouteConfig> get routes => [
+        _i3.RouteConfig(MyHomeRoute.name, path: '/'),
+        _i3.RouteConfig(AutoRouteRoute1.name, path: '/main1/gorilla'),
+        _i3.RouteConfig(AutoRouteRoute2.name, path: '/auto-route-page2'),
+        _i3.RouteConfig(AutoRouteRoute3.name, path: '/auto-route-page3'),
+        _i3.RouteConfig(FlutterSvgSample.name, path: '/flutter-svg-sample')
       ];
 }
 
 /// generated route for [_i1.MyHomePage]
-class MyHomeRoute extends _i5.PageRouteInfo<void> {
+class MyHomeRoute extends _i3.PageRouteInfo<void> {
   const MyHomeRoute() : super(name, path: '/');
 
   static const String name = 'MyHomeRoute';
 }
 
 /// generated route for [_i2.AutoRoutePage1]
-class AutoRouteRoute1 extends _i5.PageRouteInfo<void> {
+class AutoRouteRoute1 extends _i3.PageRouteInfo<void> {
   const AutoRouteRoute1() : super(name, path: '/main1/gorilla');
 
   static const String name = 'AutoRouteRoute1';
 }
 
-/// generated route for [_i3.AutoRoutePage2]
-class AutoRouteRoute2 extends _i5.PageRouteInfo<void> {
+/// generated route for [_i2.AutoRoutePage2]
+class AutoRouteRoute2 extends _i3.PageRouteInfo<void> {
   const AutoRouteRoute2() : super(name, path: '/auto-route-page2');
 
   static const String name = 'AutoRouteRoute2';
 }
 
-/// generated route for [_i4.AutoRoutePage3]
-class AutoRouteRoute3 extends _i5.PageRouteInfo<void> {
+/// generated route for [_i2.AutoRoutePage3]
+class AutoRouteRoute3 extends _i3.PageRouteInfo<void> {
   const AutoRouteRoute3() : super(name, path: '/auto-route-page3');
 
   static const String name = 'AutoRouteRoute3';
+}
+
+/// generated route for [_i2.FlutterSvgSample]
+class FlutterSvgSample extends _i3.PageRouteInfo<void> {
+  const FlutterSvgSample() : super(name, path: '/flutter-svg-sample');
+
+  static const String name = 'FlutterSvgSample';
 }
